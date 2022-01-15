94482bea7825814debe98a01977d5a6c54213478
aburizalpurnama <purnamar9@gmail.com>
2022-01-15 22:36:21 +0700

    succesfull test database connection

---
+++ spring-demo-hibernate/src/test/java/DbConnectionTest.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnectionTest {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/hb_student_tracker?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+        String username = "rizal";
+        String password = "3748";
+
+        try {
+            System.out.println("Connecting to Mysql database");
+            Connection myConn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection : " + myConn + " is successfull");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
