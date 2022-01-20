b79e272f9a22c0a71160da322fd2fd2934ae1d70
aburizalpurnama <purnamar9@gmail.com>
2022-01-20 23:42:45 +0700

    #267 Successful test database connection

---
+++ web-customer-tracker/src/main/java/com/rizal/springdemo/crm/TestDBServlet.java
@@ -0,0 +1,45 @@
+package com.rizal.springdemo.crm;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/TestDbServlet")
+public class TestDBServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // setup connection variables
+        String user = "rizal";
+        String password = "3748";
+        String url = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+        String driver = "com.mysql.jdbc.Driver";
+
+        // get connection to database
+        try {
+            PrintWriter out = resp.getWriter();
+
+            out.println("Connectiong to : " + url);
+
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            out.println("Connection Success");
+
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+
+
+    }
+}
