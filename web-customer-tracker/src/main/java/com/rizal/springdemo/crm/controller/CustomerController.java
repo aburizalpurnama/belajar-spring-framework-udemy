e76791987b84c8a26f49afe52efb0c5ac07934f5
aburizalpurnama <purnamar9@gmail.com>
2022-01-21 02:15:37 +0700

    #283 Inject DAO nto Controller

--- web-customer-tracker/src/main/java/com/rizal/springdemo/crm/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/rizal/springdemo/crm/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package com.rizal.springdemo.crm.controller;
 
+import com.rizal.springdemo.crm.dao.CustomerDao;
+import com.rizal.springdemo.crm.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // inject customerDao
+    @Autowired
+    private CustomerDao customerDao;
+
     @RequestMapping("/list")
     public String listCustomers(Model model){
+
+        // get data
+        List<Customer> customers = customerDao.getCustomers();
+
+        // add data to model
+        model.addAttribute("customers", customers);
 
         return "list-customers";
     }
