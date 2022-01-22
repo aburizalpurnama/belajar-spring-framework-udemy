b9e6c47825b19e16b5267deb040ca1631c01c82b
aburizalpurnama <purnamar9@gmail.com>
2022-01-22 21:27:53 +0700

    implement @Before advice AOP

---
+++ spring-demo-aop/src/main/java/com/rizal/spring/aop/aspect/LoginAspect.java
@@ -0,0 +1,20 @@
+package com.rizal.spring.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoginAspect {
+
+    // this is where we add all of our related for logging
+
+    // create an aspect method with @Before annotation
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice(){
+
+        System.out.println("\n===========>>>>>> Executing @Before advice on addAccount()");
+
+    }
+}
--- spring-demo-aop/src/test/java/BeforeAdviceTest.java
+++ spring-demo-aop/src/test/java/BeforeAdviceTest.java
@@ -1,4 +1,26 @@
+import com.rizal.spring.aop.config.AppConfig;
+import com.rizal.spring.aop.dao.AccountDao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BeforeAdviceTest {
 
+    public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AccountDao accountDao = context.getBean("accountDao", AccountDao.class);
+
+        accountDao.addAccount();
+        
+        accountDao.addAccount();
+
+        context.close();
+
+    }
 }
