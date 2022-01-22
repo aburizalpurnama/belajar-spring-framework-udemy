e40cf1bf99911d2e1c4847e7a8b3b2c26385be8a
aburizalpurnama <purnamar9@gmail.com>
2022-01-23 00:41:58 +0700

    #335 AOP - Declaring Reusable Pointcut

--- spring-demo-aop/src/main/java/com/rizal/spring/aop/aspect/LoginAspect.java
+++ spring-demo-aop/src/main/java/com/rizal/spring/aop/aspect/LoginAspect.java
@@ -2,16 +2,21 @@ package com.rizal.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoginAspect {
 
+    // create pointcut
+    @Pointcut("execution(* add*(..))")
+    private void forDaoPackage(){};
+
     // this is where we add all of our related for logging
 
     // create an aspect method with @Before annotation
-    @Before("execution(* add*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
 
         System.out.println("\n===========>>>>>> Executing @Before advice on addAccount()");
