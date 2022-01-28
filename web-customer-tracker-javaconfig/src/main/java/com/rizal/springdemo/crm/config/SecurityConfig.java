d8cbaec18eacd382d550266228c432e374bd095b
aburizalpurnama <purnamar9@gmail.com>
2022-01-28 14:15:05 +0700

    #512 Securing REST API Endpoints

---
+++ web-customer-tracker-javaconfig/src/main/java/com/rizal/springdemo/crm/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.rizal.springdemo.crm.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/customers").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers(HttpMethod.GET, "/api/customers/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/customers/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER")
+                .antMatchers(HttpMethod.POST, "/api/customers/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers(HttpMethod.DELETE, "/api/customers/**").hasAnyRole("ADMIN")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ web-customer-tracker-javaconfig/src/main/java/com/rizal/springdemo/crm/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.rizal.springdemo.crm.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
