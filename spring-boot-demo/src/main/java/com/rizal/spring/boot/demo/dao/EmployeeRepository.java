386736b1735e40a6886eea247d3ed5c2b2074ce9
aburizalpurnama <purnamar9@gmail.com>
2022-01-31 20:33:20 +0700

    Spring Data Rest - Specify custom plural name / path for an entity

--- spring-boot-demo/src/main/java/com/rizal/spring/boot/demo/dao/EmployeeRepository.java
+++ spring-boot-demo/src/main/java/com/rizal/spring/boot/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.rizal.spring.boot.demo.dao;
 
 import com.rizal.spring.boot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
